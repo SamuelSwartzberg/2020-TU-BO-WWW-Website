216b565f3e6e98cac699cb250019b80ff5cc4aa5
Sam Swartzberg <me@samswartzberg.com>
2020-06-23 19:12:23 +0200

    formatting improvements

--- post-formatting.js
+++ post-formatting.js
@@ -4,9 +4,15 @@ tocbot.init({
   // Where to grab the headings to build the table of contents.
   contentSelector: '#main-article',
   // Which headings to grab inside of the contentSelector element.
-  headingSelector: 'h1, h2, h3, h4, h5, h6',
+  headingSelector: 'h2, h3, h4, h5, h6',
   // For headings inside relative or absolute positioned containers within content.
   hasInnerContainers: true,
+  // How many heading levels should not be collapsed.
+// For example, number 6 will show everything since
+// there are only 6 heading levels and number 0 will collapse them all.
+// The sections that are hidden will open
+// and close as you scroll to headings within them.
+  collapseDepth: 6
 });
 
 /* from https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro */
