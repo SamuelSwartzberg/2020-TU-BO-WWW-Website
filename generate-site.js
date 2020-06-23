1d2fc4c02e63bb2e90e2fdd92bf14eff4accd57e
Sam Swartzberg <me@samswartzberg.com>
2020-06-23 02:07:59 +0200

    Split up config variables into a new file

--- generate-site.js
+++ generate-site.js
@@ -1,23 +1,3 @@
-/*
-Specify your data here!
-*/
-
-let firstName = "Sam";
-let lastName = "Swartzberg";
-let description =
-`Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-let links = /* [text, href] */[
-  ["Home page", "samswartzberg.com"],
-  ["me@samswartzberg.com", "mailto:samswartzberg.com"]]
-
-/*
-END
-*/
-
 /* insert info into document */
 let nameHeader = document.querySelector(".name-header");
 let contactLinkList = document.querySelector(".contact-link-list");
