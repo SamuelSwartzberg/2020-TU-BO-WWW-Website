f4922293b33a082b16a8f7a981629a631b9e2c57
Sam Swartzberg <me@samswartzberg.com>
2020-07-12 18:59:17 +0200

    Improve the print display some

--- js/print.js
+++ js/print.js
@@ -5,9 +5,7 @@ document.querySelector('.print-pager').onclick = () => {
     let bottomFootnote = document.querySelector("#"+item.href.split("#")[1]);
     item.outerHTML += `<span id="${bottomFootnote.id}" class="${bottomFootnote.classList.value}">${bottomFootnote.innerHTML}</span>`;
   });
-
-
-
+  document.querySelector('.footnote-container').outerHTML = "";
   window.PagedPolyfill.preview();
   Paged.registerHandlers(class extends Paged.Handler {
     constructor(chunker, polisher, caller) {
@@ -15,38 +13,39 @@ document.querySelector('.print-pager').onclick = () => {
 
     }
 
-    // afterRendered(pages) {
-    //   for (var page of pages) {
-    //     var footnotes = page.element.querySelectorAll('.footnote-bottom');
-    //     if (footnotes.length === 0) {
-    //       continue;
-    //     }
-    //
-    //     var pageContent = page.element.querySelector('.pagedjs_page_content');
-    //     var hr = document.createElement('hr');
-    //     var footnoteArea = document.createElement('div');
-    //
-    //     pageContent.style.display = 'flex';
-    //     pageContent.style.flexDirection = 'column';
-    //
-    //     hr.className = 'footnote-break';
-    //     hr.style.marginTop = 'auto';
-    //     hr.style.paddingTop = "0.5em";
-    //     hr.style.marginBottom = "1em";
-    //     hr.style.marginLeft = 0;
-    //     hr.style.marginRight = 'auto';
-    //     hr.style.width = "20%";
-    //     pageContent.appendChild(hr);
-    //
-    //     footnoteArea.className = 'footnote-area';
-    //     pageContent.appendChild(footnoteArea);
-    //
-    //     for (var footnote of footnotes) {
-    //
-    //       footnoteArea.appendChild(footnote);
-    //
-    //     }
-    //   }
-    // }
+    afterRendered(pages) {
+      for (var page of pages) {
+        var footnotes = page.element.querySelectorAll('.footnote-bottom');
+        console.log(page);
+        if (footnotes.length === 0) {
+          continue;
+        }
+
+        var pageContent = page.element.querySelector('.pagedjs_page_content');
+        var hr = document.createElement('hr');
+        var footnoteArea = document.createElement('div');
+
+        pageContent.style.display = 'flex';
+        pageContent.style.flexDirection = 'column';
+
+        hr.className = 'footnote-break';
+        hr.style.marginTop = 'auto';
+        hr.style.paddingTop = "0.5em";
+        hr.style.marginBottom = "1em";
+        hr.style.marginLeft = 0;
+        hr.style.marginRight = 'auto';
+        hr.style.width = "20%";
+        pageContent.appendChild(hr);
+
+        footnoteArea.className = 'footnote-area';
+        pageContent.appendChild(footnoteArea);
+
+        for (var footnote of footnotes) {
+
+          footnoteArea.appendChild(footnote);
+
+        }
+      }
+    }
   });
 }
