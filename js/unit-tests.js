556efe6dc0e27b8e41c447685deddbc9f1230980
Sam Swartzberg <me@samswartzberg.com>
2020-07-23 02:27:02 +0200

    implement a method to find subsequent spaces

--- js/unit-tests.js
+++ js/unit-tests.js
@@ -5,3 +5,18 @@ function testFootnoteCountMatch() {
     throw new Error(`There are ${bottomFootnoteAmount} bottom footnotes, but ${inlineFootnoteAmount} inline footnotes.`)
   }
 }
+
+function testDoubleSpace(){
+  let counter = 0;
+  document.querySelectorAll('p, ul, ol, figcaption, h1, h2, h3, h4, h5, h6, blockquote').forEach((item, i) => {
+    if (item.innerText.includes("  ")){
+      console.log(`Found double whitespace in ${item.innerText}`);
+      counter++;
+    }
+  });
+  console.log("Found " + counter + " double spaces.");
+  return counter;
+}
+
+testFootnoteCountMatch();
+testDoubleSpace();
